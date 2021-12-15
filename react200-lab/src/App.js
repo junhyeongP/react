5b848728646a6efafcedc9092a2d845a01928163
kshelp <kshelp@naver.com>
2021-12-16 00:11:06 +0900

    .

---
+++ react200-lab/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import MyComponent from './021/App';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="body">
+        <MyComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
